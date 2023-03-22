f4865a748686a6101d7c89e47dd42265ed23101c
Charles Childress <cchildress1219@gmail.com>
2023-03-22 12:49:24 -0500

    updates

--- fe/tau-rogu-fe/src/App.tsx
+++ fe/tau-rogu-fe/src/App.tsx
@@ -1,24 +1,22 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
-//asd
+
+const Post = () => {
+  return (
+    <div className='vstack'>
+      <h3>Title</h3>
+      <h4>username</h4>
+    </div>
+  );
+};
+
 function App() {
   return (
-    <div className='App'>
-      <header className='App-header'>
-        <img src={logo} className='App-logo' alt='logo' />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className='App-link'
-          href='https://reactjs.org'
-          target='_blank'
-          rel='noopener noreferrer'
-        >
-          Learn React
-        </a>
-      </header>
+    <div className='vstack center'>
+      <h1>Header</h1>
+      <Post />
+      <Post />
     </div>
   );
 }
